0583dc7f3be08bddd85767999a17d2952592d9e2
Aleksei Waddington <amw694@nyu.edu>
2020-05-10 04:25:01 -0400

    finished

---
+++ Return/Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject[] portraits;
+    public HandleTextFile textFileHandler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //textFileHandler.readTextFile("Save");
+        Debug.Log(Application.dataPath);
+        getPotentialIndexFromFile();
+        //test();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    void getPotentialIndexFromFile()
+    {
+        string potentialIndex = textFileHandler.readTextFile(Application.dataPath + "/Resources/Save.txt");
+
+        if (potentialIndex == null)
+        {
+            GetNewIndex();
+        }
+        else
+        {
+            activatePortrait(System.Convert.ToInt32(potentialIndex));
+        }
+    }
+
+    void test()
+    {
+        int index = Random.Range(0, portraits.Length);
+        activatePortrait(index);
+    }
+
+    void GetNewIndex()
+    {
+        int index = Random.Range(0, portraits.Length);
+        textFileHandler.writeTextFile(Application.dataPath + "/Resources/Save.txt", index.ToString());
+        activatePortrait(index);
+    }
+
+    void activatePortrait(int index)
+    {
+        GameObject portrait = portraits[index];
+        portrait.SetActive(true);
+    }
+}
---
+++ Return/Assets/Scripts/HandleTextFile.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class HandleTextFile: MonoBehaviour
+{
+    private string index;
+    public string readTextFile(string file_path)
+    {
+        //var file = Resources.Load(file_path) as TextAsset;
+
+
+        //if(file.text.Trim().Equals("".Trim()))
+        //{
+        //    return null;
+        //}
+
+        //return file.text;
+        if (!File.Exists(file_path))
+        {
+            File.Create(file_path).Dispose();
+        }
+
+        StreamReader inp_stm = new StreamReader(file_path);
+        while (!inp_stm.EndOfStream)
+        {
+            string inp_ln = inp_stm.ReadLine();
+            index = inp_ln;
+            // Do Something with the input. 
+        }
+
+        inp_stm.Close();
+
+        return index;
+
+    }
+
+    public void writeTextFile(string file_path, string value)
+    {
+        Debug.Log(value);
+        StreamWriter writer = new StreamWriter(file_path, true);
+        Debug.Log(writer);
+        writer.WriteLine(value);
+        writer.Close();
+    }
+
+}
