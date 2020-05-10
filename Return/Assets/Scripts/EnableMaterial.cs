68414e72c8c15499f57497f118518f57b8ba743c
Aleksei Waddington <amw694@nyu.edu>
2020-05-09 20:14:27 -0400

    gitignore

---
+++ Return/Assets/Scripts/EnableMaterial.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnableMaterial : MonoBehaviour
+{
+
+    private bool _disabledMaterial = false;
+    public Image image;
+    public Material scrabledMat;
+    public Material defaultMat;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            if(!_disabledMaterial)
+            {
+                DisableShader();
+                _disabledMaterial = true;
+            }
+        }
+        else if(_disabledMaterial)
+        {
+            EnableShader();
+            _disabledMaterial = false;
+        }
+    }
+
+    void DisableShader()
+    {
+        image.material = defaultMat;
+    }
+
+    void EnableShader()
+    {
+        image.material = scrabledMat;
+    }
+}
---
+++ Return/Assets/Scripts/UpdateShadow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UpdateShadow : MonoBehaviour
+{
+    public RectTransform portrait;
+    public RectTransform shadow;
+
+    public float xOffset = 0f;
+    public float yOffset = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shadow.sizeDelta = new Vector2(portrait.sizeDelta.x, portrait.sizeDelta.y);
+        //shadow.sizeDelta.y = portrait.sizeDelta.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shadow.anchoredPosition = new Vector2(portrait.anchoredPosition.x + xOffset, portrait.anchoredPosition.y + yOffset);
+        //shadow.anchoredPosition.x = portrait.anchoredPosition.x + xOffset;
+        //shadow.anchoredPosition.y = portrait.anchoredPosition.y + yOffset;
+    }
+}
